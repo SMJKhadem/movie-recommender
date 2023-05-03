1628df620111417d6cba2d5ba2fff5c19e78f026
SMJKhadem <jebreiilkhadem3@gmail.com>
2023-05-03 14:14:56 +0200

    all

--- utils.py
+++ utils.py
@@ -8,7 +8,7 @@ warnings.filterwarnings(action='ignore')
 with open('./factorizer_NMF.pkl','rb') as file_in:
               model = pickle.load(file_in)
     # database
-user_item=pd.read_csv('/Users/kasiafalecka/Desktop/spiced_projects/nigela-network-student-code/week_10/project/user_item.csv', index_col=0)
+user_item=pd.read_csv('./user_item.csv', index_col=0)
 MOVIES=user_item.columns.to_list()
 USERS=user_item.index.to_list()
 col_mean= user_item.mean()
